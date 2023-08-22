c6819942e07f5ae161a70d252e7e458453b634a8
Jalil07 <joenardson.jd@gmail.com>
2023-08-22 17:45:53 +0300

    fjdmfm

--- lib/pages/other_links.dart
+++ lib/pages/other_links.dart
@@ -115,12 +115,17 @@ class _OtherLinksPageState extends State<OtherLinksPage> {
             color: Colors.white,
             child: Row(
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 15),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  margin: const EdgeInsets.only(left: 15),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFFFA980B),
+                  ),
                   child: Image.asset(
                     'assets/images/image4.png',
-                    height: 45,
-                    width: 45,
+                    height: 30,
+                    width: 30,
                   ),
                 ),
                 const SizedBox(width: 15),
